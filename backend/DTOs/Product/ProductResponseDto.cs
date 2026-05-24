a1d5d0026d3f24b540fbed6841bcb071f93d9338
Avinash Shukla <avishukla3010@gmail.com>
2026-05-24 21:22:32 +0530

    add ProductResponseDto class for product data transfer

---
+++ backend/DTOs/Product/ProductResponseDto.cs
@@ -0,0 +1,12 @@
+namespace ShopSphere.DTOs.Product
+{
+    public class ProductResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+
+    
+}

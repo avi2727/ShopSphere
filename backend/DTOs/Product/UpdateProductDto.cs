40210614f30b319aca0958747b7d03573db56b2d
Avinash Shukla <avishukla3010@gmail.com>
2026-05-24 21:22:45 +0530

    Add UpdateProductDto class for updating product details

---
+++ backend/DTOs/Product/UpdateProductDto.cs
@@ -0,0 +1,10 @@
+namespace ShopSphere.DTOs.Product
+{
+    public class UpdateProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
